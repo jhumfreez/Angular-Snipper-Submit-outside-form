85c0a29fdb95827f1bcaa62af4c2c1b4c73137fc
jhumfreez <25644264+jhumfreez@users.noreply.github.com>
2022-12-08 05:32:46 -0600

    1 file modified

--- src/app/submit.directive.ts
+++ src/app/submit.directive.ts
@@ -9,8 +9,9 @@ export class SubmitDirective {
    * A: This direct applies to buttons of type "Submit". Natively, the submit button is
    * associated with a form with a matching id.
    *
-   * So, one option is to match the button id to the form. This is probably
-   * super unclear though.
+   * So, one option is to match the button id to the form.
+   * This is probably super unclear though (and may depend on browser implementation).
+   * 
    * An alternative is getting the form element and call the native requestSubmit
    *
    * resoures:
